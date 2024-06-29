cf84f94023ba97038d77987e1a7e2aa4aabd5dc4
V-G-Spec <vanshg2001@protonmail.com>
2024-06-29 23:01:18 +0200

    FCN runs

--- src/models/FCN/FCN.py
+++ src/models/FCN/FCN.py
@@ -6,18 +6,29 @@ import numpy as np
 import torch
 from matplotlib import pyplot as plt
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.models as models
 from tqdm import tqdm
 import torch.optim as optim
 
 
+def conv_relu(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+        nn.ReLU(inplace=True),
+    )
 
 
-@MODEL_REGISTRY.register('fcn_8')
+def max_pool(kernel_size=2, stride=2):
+    return nn.MaxPool2d(kernel_size, stride)
+
+
+@MODEL_REGISTRY.register("fcn_8")
 class FCN8s(nn.Module):
     """Using pre-trained VGG for base model
     Source model: https://arxiv.org/pdf/1411.4038"""
-    def __init__(self, num_classes=21):
+
+    def __init__(self, num_classes=1):
         super(FCN8s, self).__init__()
 
         # Load the pretrained VGG16 model
@@ -39,13 +50,17 @@ class FCN8s(nn.Module):
 
         # Transposed convolutions for upsampling
         self.upscore2 = nn.ConvTranspose2d(
-            num_classes, num_classes, kernel_size=4, stride=2, padding=1)
+            num_classes, num_classes, kernel_size=4, stride=2, padding=1
+        )
         self.upscore_pool4 = nn.ConvTranspose2d(
-            num_classes, num_classes, kernel_size=4, stride=2, padding=1)
+            num_classes, num_classes, kernel_size=4, stride=2, padding=1
+        )
         self.upscore8 = nn.ConvTranspose2d(
-            num_classes, num_classes, kernel_size=16, stride=8, padding=4)
+            num_classes, num_classes, kernel_size=16, stride=8, padding=4
+        )
         self.upscore16 = nn.ConvTranspose2d(
-            num_classes, num_classes, kernel_size=32, stride=16, bias=False)
+            num_classes, num_classes, kernel_size=32, stride=16, bias=False
+        )
 
         # Scoring layers for skip connections
         self.score_pool3 = nn.Conv2d(256, num_classes, kernel_size=1)
@@ -71,21 +86,39 @@ class FCN8s(nn.Module):
 
         # Has twice the spatial resolution of score_fr
         upscore2 = self.upscore2(score_fr)
-        score_pool4 = self.score_pool4(
-            pool4)[:, :, 5:5 + upscore2.size()[2], 5:5 + upscore2.size()[3]]
-        upscore_pool4 = (upscore2 + score_pool4)  # Check if necessary
+        score_pool4 = self.score_pool4(pool4)
+        score_pool4 = F.interpolate(
+            score_pool4, size=upscore2.shape[2:], mode="bilinear", align_corners=True
+        )
+        upscore_pool4 = upscore2 + score_pool4  # Check if necessary
         # Stop here for FCN16s
 
         upscore_pool16 = self.upscore16(upscore_pool4)
-        score_pool3 = self.score_pool3(
-            pool3)[:, :, 9:9 + upscore_pool16.size()[2], 9:9 + upscore_pool16.size()[3]]
+        score_pool3 = self.score_pool3(pool3)
+        score_pool3 = F.interpolate(
+            score_pool3,
+            size=upscore_pool16.shape[2:],
+            mode="bilinear",
+            align_corners=True,
+        )
         upscore8 = self.upscore8(upscore_pool16 + score_pool3)
-        out = upscore8[:, :, 31:31 + x.size()[2], 31:31 + x.size()[3]]
+        out = F.interpolate(
+            upscore8,
+            size=(x.size()[2], x.size()[3]),
+            mode="bilinear",
+            align_corners=True,
+        )
+        min_val, max_val = (torch.min(out), torch.max(out))
+        out = (out - min_val) / (max_val - min_val)
 
+        # out = upscore8[:, 31 : 31 + , 31 : 31 + x.size()[3]]
+        # print(out.shape)
         return out
 
 
-def train(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs):
+def train(
+    train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs
+):
     # Initialize lists to keep track of metrics
     train_loss_history = []
     val_loss_history = []
@@ -97,62 +130,73 @@ def train(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimiz
         model.train()
         running_loss = 0.0
         running_metrics = {metric: 0.0 for metric in metric_fns}
-        
+
         for i, (inputs, targets) in enumerate(train_dataloader):
             # Zero the parameter gradients
             optimizer.zero_grad()
-            
+
             # Forward pass
             outputs = model(inputs)
             loss = loss_fn(outputs, targets)
-            
+            # raise
             # Backward pass and optimize
             loss.backward()
             optimizer.step()
-            
+
             # Log partial metrics
             running_loss += loss.item()
             for metric_name, metric_fn in metric_fns.items():
                 running_metrics[metric_name] += metric_fn(outputs, targets).item()
 
-
         # Collect metrics at the end of the epoch
         train_loss_history.append(running_loss / len(train_dataloader))
         for metric_name in metric_fns:
-            train_metrics_history[metric_name].append(running_metrics[metric_name] / len(train_dataloader))
+            train_metrics_history[metric_name].append(
+                running_metrics[metric_name] / len(train_dataloader)
+            )
 
         # Validation phase
         model.eval()
         val_running_loss = 0.0
         val_running_metrics = {metric: 0.0 for metric in metric_fns}
-        
+
         with torch.no_grad():
             for inputs, targets in eval_dataloader:
                 outputs = model(inputs)
                 loss = loss_fn(outputs, targets)
-                
+
                 val_running_loss += loss.item()
                 for metric_name, metric_fn in metric_fns.items():
-                    val_running_metrics[metric_name] += metric_fn(outputs, targets).item()
+                    val_running_metrics[metric_name] += metric_fn(
+                        outputs, targets
+                    ).item()
 
         # Log partial metrics for validation
         val_loss_history.append(val_running_loss / len(eval_dataloader))
         for metric_name in metric_fns:
-            val_metrics_history[metric_name].append(val_running_metrics[metric_name] / len(eval_dataloader))
+            val_metrics_history[metric_name].append(
+                val_running_metrics[metric_name] / len(eval_dataloader)
+            )
 
         # Log to TensorBoard
-        
-        print(f'Epoch {epoch+1}/{n_epochs}, Train Loss: {train_loss_history[-1]}, Validation Loss: {val_loss_history[-1]}')
 
-    print('Finished Training, saving model.')
-    model_path = './models/fcn_8/checkpoints'
+        print(
+            f"Epoch {epoch+1}/{n_epochs}, Train Loss: {train_loss_history[-1]}, Validation Loss: {val_loss_history[-1]}"
+        )
+
+    print("Finished Training, saving model.")
+    model_path = "./models/fcn_8/checkpoints"
     if not os.path.exists(model_path):
         os.mkdir(model_path)
-    torch.save({
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict()
-                }, model_path + f'/model_{time.strftime("%Y%m%d-%H%M%S")}.pth')
-    print('Model saved.')
+    torch.save(
+        {
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+        },
+        model_path + f'/model_{time.strftime("%Y%m%d-%H%M%S")}.pth',
+    )
+    print("Model saved.")
+
 
 #     return model, train_loss_history, val_loss_history, train_metrics_history, val_metrics_history
 
@@ -162,7 +206,7 @@ def train(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimiz
 #     # Batch size 1, 3 color channels, 224x224 image
 #     input_tensor = torch.randn(1, 3, 224, 224)
 #     output = model(input_tensor)
-#     print(output.shape) 
+#     print(output.shape)
 
 #     # test-train
 #     loss_fn = nn.CrossEntropyLoss()
