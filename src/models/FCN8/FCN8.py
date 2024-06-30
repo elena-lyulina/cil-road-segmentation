8db2bd81402d5dd9937afba9e36f64a5ae86782c
V-G-Spec <vanshg2001@protonmail.com>
2024-06-30 09:47:10 +0200

    First draft of dino + renames

--- src/models/FCN8/FCN8.py
+++ src/models/FCN8/FCN8.py
@@ -116,106 +116,83 @@ class FCN8s(nn.Module):
         return out
 
 
-def train(
-    train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs
-):
-    # Initialize lists to keep track of metrics
-    train_loss_history = []
-    val_loss_history = []
-    train_metrics_history = {metric: [] for metric in metric_fns}
-    val_metrics_history = {metric: [] for metric in metric_fns}
+# def train(
+#     train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs
+# ):
+#     # Initialize lists to keep track of metrics
+#     train_loss_history = []
+#     val_loss_history = []
+#     train_metrics_history = {metric: [] for metric in metric_fns}
+#     val_metrics_history = {metric: [] for metric in metric_fns}
 
-    for epoch in range(n_epochs):
-        # Training phase
-        model.train()
-        running_loss = 0.0
-        running_metrics = {metric: 0.0 for metric in metric_fns}
+#     for epoch in range(n_epochs):
+#         # Training phase
+#         model.train()
+#         running_loss = 0.0
+#         running_metrics = {metric: 0.0 for metric in metric_fns}
 
-        for i, (inputs, targets) in enumerate(train_dataloader):
-            # Zero the parameter gradients
-            optimizer.zero_grad()
+#         for i, (inputs, targets) in enumerate(train_dataloader):
+#             # Zero the parameter gradients
+#             optimizer.zero_grad()
 
-            # Forward pass
-            outputs = model(inputs)
-            loss = loss_fn(outputs, targets)
-            # raise
-            # Backward pass and optimize
-            loss.backward()
-            optimizer.step()
+#             # Forward pass
+#             outputs = model(inputs)
+#             loss = loss_fn(outputs, targets)
+#             # raise
+#             # Backward pass and optimize
+#             loss.backward()
+#             optimizer.step()
 
-            # Log partial metrics
-            running_loss += loss.item()
-            for metric_name, metric_fn in metric_fns.items():
-                running_metrics[metric_name] += metric_fn(outputs, targets).item()
+#             # Log partial metrics
+#             running_loss += loss.item()
+#             for metric_name, metric_fn in metric_fns.items():
+#                 running_metrics[metric_name] += metric_fn(outputs, targets).item()
 
-        # Collect metrics at the end of the epoch
-        train_loss_history.append(running_loss / len(train_dataloader))
-        for metric_name in metric_fns:
-            train_metrics_history[metric_name].append(
-                running_metrics[metric_name] / len(train_dataloader)
-            )
+#         # Collect metrics at the end of the epoch
+#         train_loss_history.append(running_loss / len(train_dataloader))
+#         for metric_name in metric_fns:
+#             train_metrics_history[metric_name].append(
+#                 running_metrics[metric_name] / len(train_dataloader)
+#             )
 
-        # Validation phase
-        model.eval()
-        val_running_loss = 0.0
-        val_running_metrics = {metric: 0.0 for metric in metric_fns}
+#         # Validation phase
+#         model.eval()
+#         val_running_loss = 0.0
+#         val_running_metrics = {metric: 0.0 for metric in metric_fns}
 
-        with torch.no_grad():
-            for inputs, targets in eval_dataloader:
-                outputs = model(inputs)
-                loss = loss_fn(outputs, targets)
+#         with torch.no_grad():
+#             for inputs, targets in eval_dataloader:
+#                 outputs = model(inputs)
+#                 loss = loss_fn(outputs, targets)
 
-                val_running_loss += loss.item()
-                for metric_name, metric_fn in metric_fns.items():
-                    val_running_metrics[metric_name] += metric_fn(
-                        outputs, targets
-                    ).item()
+#                 val_running_loss += loss.item()
+#                 for metric_name, metric_fn in metric_fns.items():
+#                     val_running_metrics[metric_name] += metric_fn(
+#                         outputs, targets
+#                     ).item()
 
-        # Log partial metrics for validation
-        val_loss_history.append(val_running_loss / len(eval_dataloader))
-        for metric_name in metric_fns:
-            val_metrics_history[metric_name].append(
-                val_running_metrics[metric_name] / len(eval_dataloader)
-            )
+#         # Log partial metrics for validation
+#         val_loss_history.append(val_running_loss / len(eval_dataloader))
+#         for metric_name in metric_fns:
+#             val_metrics_history[metric_name].append(
+#                 val_running_metrics[metric_name] / len(eval_dataloader)
+#             )
 
-        # Log to TensorBoard
+#         # Log to TensorBoard
 
-        print(
-            f"Epoch {epoch+1}/{n_epochs}, Train Loss: {train_loss_history[-1]}, Validation Loss: {val_loss_history[-1]}"
-        )
+#         print(
+#             f"Epoch {epoch+1}/{n_epochs}, Train Loss: {train_loss_history[-1]}, Validation Loss: {val_loss_history[-1]}"
+#         )
 
-    print("Finished Training, saving model.")
-    model_path = "./models/fcn_8/checkpoints"
-    if not os.path.exists(model_path):
-        os.mkdir(model_path)
-    torch.save(
-        {
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optimizer.state_dict(),
-        },
-        model_path + f'/model_{time.strftime("%Y%m%d-%H%M%S")}.pth',
-    )
-    print("Model saved.")
-
-
-#     return model, train_loss_history, val_loss_history, train_metrics_history, val_metrics_history
-
-
-# def test():
-#     model = FCN8s(num_classes=21)
-#     # Batch size 1, 3 color channels, 224x224 image
-#     input_tensor = torch.randn(1, 3, 224, 224)
-#     output = model(input_tensor)
-#     print(output.shape)
-
-#     # test-train
-#     loss_fn = nn.CrossEntropyLoss()
-#     optimizer = optim.Adam(model.parameters(), lr=0.001)
-#     metric_fns = {
-#         'accuracy': lambda outputs, targets: (outputs.argmax(1) == targets).float().mean()
-#     }
-#     train_dataloader = ...  # Replace with actual DataLoader
-#     eval_dataloader = ...   # Replace with actual DataLoader
-
-#     n_epochs = 10
-#     train(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs)
+#     print("Finished Training, saving model.")
+#     model_path = "./models/fcn_8/checkpoints"
+#     if not os.path.exists(model_path):
+#         os.mkdir(model_path)
+#     torch.save(
+#         {
+#             "model_state_dict": model.state_dict(),
+#             "optimizer_state_dict": optimizer.state_dict(),
+#         },
+#         model_path + f'/model_{time.strftime("%Y%m%d-%H%M%S")}.pth',
+#     )
+#     print("Model saved.")
---
+++ src/models/dinoPlus/dinoPlus.py
@@ -0,0 +1,85 @@
+"""
+Partially taken from https://www.towardsdeeplearning.com/dinov2-for-custom-dataset-segmentation-a-comprehensive-tutorial/
+"""
+
+from src.models.utils import MODEL_REGISTRY
+import torch
+from transformers import Dinov2Model, Dinov2PreTrainedModel
+from transformers.modeling_outputs import SemanticSegmenterOutput
+
+
+class LinearClassifier(torch.nn.Module):
+    def __init__(self, in_channels, tokenW=32, tokenH=32, num_labels=1):
+        super(LinearClassifier, self).__init__()
+
+        self.in_channels = in_channels
+        self.width = tokenW
+        self.height = tokenH
+        self.classifier = torch.nn.Conv2d(in_channels, num_labels, (1, 1))
+
+    def forward(self, embeddings):
+        embeddings = embeddings.reshape(-1, self.height, self.width, self.in_channels)
+        embeddings = embeddings.permute(0, 3, 1, 2)
+
+        return self.classifier(embeddings)
+
+
+@MODEL_REGISTRY.register("dino_plus")
+class Dinov2ForSemanticSegmentation(Dinov2PreTrainedModel):
+    def __init__(self, config="facebook/dinov2-base"):
+        super().__init__(config)
+
+        self.dinov2 = Dinov2Model(config)
+        self.classifier = LinearClassifier(
+            config.hidden_size, 32, 32, config.num_labels
+        )
+        self._freeze_dinov2_parameters()
+
+    def _freeze_dinov2_parameters(self):
+        for name, param in self.named_parameters():
+            if name.startswith("dinov2"):
+                param.requires_grad = False
+
+    def forward(
+        self,
+        pixel_values,
+        output_hidden_states=False,
+        output_attentions=False,
+        labels=None,
+    ):
+        # use frozen features
+        outputs = self.dinov2(
+            pixel_values,
+            output_hidden_states=output_hidden_states,
+            output_attentions=output_attentions,
+        )
+        # get the patch embeddings - so we exclude the CLS token
+        patch_embeddings = outputs.last_hidden_state[:, 1:, :]
+        print(patch_embeddings.shape)
+        raise
+        # convert to logits and upsample to the size of the pixel values
+        logits = self.classifier(patch_embeddings)
+        logits = torch.nn.functional.interpolate(
+            logits, size=pixel_values.shape[2:], mode="bilinear", align_corners=False
+        )
+
+        return logits
+
+
+# # We can instantiate the model as follows:
+
+# model = Dinov2ForSemanticSegmentation.from_pretrained(
+#     "facebook/dinov2-base", id2label=id2label, num_labels=len(id2label)
+# )
+
+# # Important: we don't want to train the DINOv2 backbone, only the linear classification head. Hence we don't want to track any gradients for the backbone parameters. This will greatly save us in terms of memory used:
+
+# for name, param in model.named_parameters():
+#     if name.startswith("dinov2"):
+#         param.requires_grad = False
+
+# # Let's perform a forward pass on a random batch, to verify the shape of the logits, verify we can calculate a loss:
+
+# outputs = model(pixel_values=batch["pixel_values"], labels=batch["labels"])
+# print(outputs.logits.shape)
+# print(outputs.loss)
