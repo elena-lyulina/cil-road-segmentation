a4d68266919ab77899a726a0d15e0a27821aaeaa
louis <louis.niederloehner@gmail.com>
2024-07-02 16:32:12 +0200

    Add LinkNet and DiceBCELoss

--- src/models/LinkNet/linkNet.py
+++ src/models/LinkNet/linkNet.py
@@ -15,29 +15,6 @@ from src.models.utils import MODEL_REGISTRY
 nonlinearity = partial(F.relu, inplace=True)
 
 
-class Dblock(nn.Module):
-    def __init__(self, channel):
-        super(Dblock, self).__init__()
-        self.dilate1 = nn.Conv2d(channel, channel, kernel_size=3, dilation=1, padding=1)
-        self.dilate2 = nn.Conv2d(channel, channel, kernel_size=3, dilation=2, padding=2)
-        self.dilate3 = nn.Conv2d(channel, channel, kernel_size=3, dilation=4, padding=4)
-        self.dilate4 = nn.Conv2d(channel, channel, kernel_size=3, dilation=8, padding=8)
-        # self.dilate5 = nn.Conv2d(channel, channel, kernel_size=3, dilation=16, padding=16)
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-                if m.bias is not None:
-                    m.bias.data.zero_()
-
-    def forward(self, x):
-        dilate1_out = nonlinearity(self.dilate1(x))
-        dilate2_out = nonlinearity(self.dilate2(dilate1_out))
-        dilate3_out = nonlinearity(self.dilate3(dilate2_out))
-        dilate4_out = nonlinearity(self.dilate4(dilate3_out))
-        # dilate5_out = nonlinearity(self.dilate5(dilate4_out))
-        out = x + dilate1_out + dilate2_out + dilate3_out + dilate4_out  # + dilate5_out
-        return out
-
-
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, n_filters):
         super(DecoderBlock, self).__init__()
@@ -67,13 +44,14 @@ class DecoderBlock(nn.Module):
         return x
 
 
-@MODEL_REGISTRY.register("dLinkNet")
-class DinkNet34(nn.Module):
-    def __init__(self, num_classes=1, num_channels=3):
-        super(DinkNet34, self).__init__()
+@MODEL_REGISTRY.register('linkNet')
+class LinkNet34(nn.Module):
+    def __init__(self, num_classes=1):
+        super(LinkNet34, self).__init__()
 
         filters = [64, 128, 256, 512]
         resnet = models.resnet34(pretrained=True)
+
         self.firstconv = resnet.conv1
         self.firstbn = resnet.bn1
         self.firstrelu = resnet.relu
@@ -83,18 +61,16 @@ class DinkNet34(nn.Module):
         self.encoder3 = resnet.layer3
         self.encoder4 = resnet.layer4
 
-        self.dblock = Dblock(512)
-
         self.decoder4 = DecoderBlock(filters[3], filters[2])
         self.decoder3 = DecoderBlock(filters[2], filters[1])
         self.decoder2 = DecoderBlock(filters[1], filters[0])
         self.decoder1 = DecoderBlock(filters[0], filters[0])
 
-        self.finaldeconv1 = nn.ConvTranspose2d(filters[0], 32, 4, 2, 1)
+        self.finaldeconv1 = nn.ConvTranspose2d(filters[0], 32, 3, stride=2)
         self.finalrelu1 = nonlinearity
-        self.finalconv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.finalconv2 = nn.Conv2d(32, 32, 3)
         self.finalrelu2 = nonlinearity
-        self.finalconv3 = nn.Conv2d(32, num_classes, 3, padding=1)
+        self.finalconv3 = nn.Conv2d(32, num_classes, 2, padding=1)
 
     def forward(self, x):
         # Encoder
@@ -107,15 +83,11 @@ class DinkNet34(nn.Module):
         e3 = self.encoder3(e2)
         e4 = self.encoder4(e3)
 
-        # Center
-        e4 = self.dblock(e4)
-
         # Decoder
         d4 = self.decoder4(e4) + e3
         d3 = self.decoder3(d4) + e2
         d2 = self.decoder2(d3) + e1
         d1 = self.decoder1(d2)
-
         out = self.finaldeconv1(d1)
         out = self.finalrelu1(out)
         out = self.finalconv2(out)
@@ -123,4 +95,3 @@ class DinkNet34(nn.Module):
         out = self.finalconv3(out)
 
         return F.sigmoid(out)
-
