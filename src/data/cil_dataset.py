9110f8924dbe1cda4cfe0d3d34a885e81fdc0842
V-G-Spec <vanshg2001@protonmail.com>
2024-07-04 22:34:48 +0200

    Untested data augmentation included

--- src/data/cil_dataset.py
+++ src/data/cil_dataset.py
@@ -5,7 +5,9 @@ import cv2
 import numpy as np
 from PIL import Image
 from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+import torchvision.transforms as T
+import torch
 
 from src.constants import DATA_PATH, DEVICE, CUTOFF, PATCH_SIZE
 from src.data.datahandler import DATAHANDLER_REGISTRY, DataHandler
@@ -20,21 +22,65 @@ class CILDataHandler(DataHandler):
     train_images_path = train_path.joinpath("images")
     train_masks_path = train_path.joinpath("groundtruth")
 
-    def __init__(self, batch_size=4, shuffle=True, resize_to=(384, 384)):
+    def __init__(self, batch_size=4, shuffle=True, resize_to=(384, 384), augment=False):
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.resize_to = resize_to
+        self.augment = augment
 
-        images_paths = [f for f in sorted(glob(str(self.train_images_path) + '/*.png'))]
-        masks_paths = [f for f in sorted(glob(str(self.train_masks_path) + '/*.png'))]
+        images_paths = [f for f in sorted(glob(str(self.train_images_path) + "/*.png"))]
+        masks_paths = [f for f in sorted(glob(str(self.train_masks_path) + "/*.png"))]
 
-        self.train_image_paths, self.val_image_paths, self.train_mask_paths, self.val_mask_paths = train_test_split(
-            images_paths, masks_paths, test_size=0.2, random_state=42
-        )
+        (
+            self.train_image_paths,
+            self.val_image_paths,
+            self.train_mask_paths,
+            self.val_mask_paths,
+        ) = train_test_split(images_paths, masks_paths, test_size=0.2, random_state=42)
 
     def get_train_val_dataloaders(self) -> Tuple[DataLoader, DataLoader]:
-        train_dataset = CILDataset(self.train_image_paths, self.train_mask_paths, PATCH_SIZE, CUTOFF, DEVICE, resize_to=self.resize_to)
-        val_dataset = CILDataset(self.val_image_paths, self.val_mask_paths, PATCH_SIZE, CUTOFF, DEVICE, resize_to=self.resize_to)
+        train_dataset = CILDataset(
+            self.train_image_paths,
+            self.train_mask_paths,
+            PATCH_SIZE,
+            CUTOFF,
+            DEVICE,
+            resize_to=self.resize_to,
+            augment=False,
+        )
+
+        if self.augment:
+            geometric_dataset = CILDataset(
+                self.train_image_paths,
+                self.train_mask_paths,
+                PATCH_SIZE,
+                CUTOFF,
+                DEVICE,
+                resize_to=self.resize_to,
+                augment="geometric",
+            )
+            color_dataset = CILDataset(
+                self.train_image_paths,
+                self.train_mask_paths,
+                PATCH_SIZE,
+                CUTOFF,
+                DEVICE,
+                resize_to=self.resize_to,
+                augment="color",
+            )
+            train_dataset = ConcatDataset(
+                [train_dataset, geometric_dataset, color_dataset]
+            )
+
+        val_dataset = CILDataset(
+            self.val_image_paths,
+            self.val_mask_paths,
+            PATCH_SIZE,
+            CUTOFF,
+            DEVICE,
+            resize_to=self.resize_to,
+            augment=False,
+        )
 
         train_dataloader = DataLoader(train_dataset, self.batch_size, self.shuffle)
         val_dataloader = DataLoader(val_dataset, self.batch_size, self.shuffle)
@@ -46,32 +92,67 @@ class CILDataHandler(DataHandler):
 class CILDataset(Dataset):
     # dataset class that deals with loading the data and making it available by index.
 
-    def __init__(self, image_paths, mask_paths, patch_size, cutoff, device, resize_to=(400, 400)):
+    def __init__(
+        self,
+        image_paths,
+        mask_paths,
+        patch_size,
+        cutoff,
+        device,
+        resize_to=(400, 400),
+        augment=False,
+    ):
         self.items = list(zip(image_paths, mask_paths))
         self.patch_size = patch_size
         self.cutoff = cutoff
         self.device = device
         self.resize_to = resize_to
+        self.augment = augment
+
+        self.geometric_transform = T.Compose(
+            [
+                T.RandomRotation(30),
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+            ]
+        )
+
+        self.color_transform = T.Compose(
+            [
+                T.ColorJitter(brightness=0.2, contrast=0.2),
+                T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+            ]
+        )
 
     def _preprocess(self, x, y):
         # to keep things simple we will not apply transformations to each sample,
         # but it would be a very good idea to look into preprocessing
+        if self.augment == "geometric":
+            x = self.geometric_transform(x)
+        elif self.augment == "color":
+            x = self.color_transform(x)
         return x, y
 
     def __getitem__(self, item):
         # return self._preprocess(np_to_tensor(self.x[item], self.device), np_to_tensor(self.y[[item]], self.device))
         img_path, mask_path = self.items[item]
-        image = np.array(Image.open(img_path))[:, :, :3].astype(np.float32) / 255.
-        mask = np.array(Image.open(mask_path).convert('L')).astype(np.float32) / 255.
+        image = np.array(Image.open(img_path))[:, :, :3].astype(np.float32) / 255.0
+        mask = np.array(Image.open(mask_path).convert("L")).astype(np.float32) / 255.0
 
         if self.resize_to != (image.shape[0], image.shape[1]):  # resize images
             image = cv2.resize(image, dsize=self.resize_to)
             mask = cv2.resize(mask, dsize=self.resize_to)
 
-        image = np.moveaxis(image, -1, 0)  # pytorch works with CHW format instead of HWC
+        image = np.moveaxis(
+            image, -1, 0
+        )  # pytorch works with CHW format instead of HWC
         mask = np.reshape(mask, (mask.shape[0], mask.shape[1], 1))
         mask = np.moveaxis(mask, -1, 0)  # pytorch works with CHW format instead of HWC
-        return self._preprocess(np_to_tensor(image, self.device), np_to_tensor(mask, self.device))
+
+        image_tensor = np_to_tensor(image, self.device)
+        mask_tensor = np_to_tensor(mask, self.device)
+
+        return self._preprocess(image_tensor, mask_tensor)
 
     def __len__(self):
         return len(self.items)
--- src/data/massachusetts_dataset.py
+++ src/data/massachusetts_dataset.py
@@ -3,6 +3,8 @@ from glob import glob
 from typing import Tuple
 
 import numpy as np
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+import torchvision.transforms as T
 import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
@@ -22,46 +24,99 @@ class MassachusettsDataHandler(DataHandler):
     test_images_path = dataset_path.joinpath("test")
     test_masks_path = dataset_path.joinpath("test_labels")
 
-    def __init__(self, batch_size=32, num_workers=16, pin_memory=True, shuffle=True):
+    def __init__(
+        self,
+        batch_size=32,
+        num_workers=16,
+        pin_memory=True,
+        shuffle=True,
+        augment=False,
+    ):
         # num_workers: tune that to CPU capacities, keep high number of parallelization used
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.pin_memory = pin_memory
         self.shuffle = shuffle
+        self.augment = augment
 
     def get_train_val_dataloaders(self) -> Tuple[DataLoader, DataLoader]:
-        train_dataset = MassachusettsDataset(self.train_images_path, self.train_masks_path)
-        val_dataset = MassachusettsDataset(self.test_images_path, self.test_masks_path)
+        train_dataset = MassachusettsDataset(
+            self.train_images_path, self.train_masks_path, augment=False
+        )
 
-        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=self.batch_size,
-                                                       num_workers=self.num_workers,
-                                                       pin_memory=self.pin_memory, shuffle=self.shuffle)
-        val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=self.batch_size,
-                                                     num_workers=self.num_workers,
-                                                     pin_memory=self.pin_memory, shuffle=self.shuffle)
+        if self.augment:
+            geometric_dataset = MassachusettsDataset(
+                self.train_images_path, self.train_masks_path, augment="geometric"
+            )
+            color_dataset = MassachusettsDataset(
+                self.train_images_path, self.train_masks_path, augment="color"
+            )
+            train_dataset = ConcatDataset(
+                [train_dataset, geometric_dataset, color_dataset]
+            )
+
+        val_dataset = MassachusettsDataset(
+            self.test_images_path, self.test_masks_path, augment=False
+        )
+
+        train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=self.shuffle,
+        )
+        val_dataloader = DataLoader(
+            val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=self.shuffle,
+        )
 
         return train_dataloader, val_dataloader
 
 
 @DATASET_REGISTRY.register("massachusetts")
 class MassachusettsDataset(Dataset):
-    def __init__(self, image_dir, mask_dir):
-        self.image_paths = sorted(glob(os.path.join(image_dir, '*.tiff')))
-        self.mask_paths = sorted(glob(os.path.join(mask_dir, '*.tif')))
+    def __init__(self, image_dir, mask_dir, augment=False):
+        self.image_paths = sorted(glob(os.path.join(image_dir, "*.tiff")))
+        self.mask_paths = sorted(glob(os.path.join(mask_dir, "*.tif")))
 
         self.crop_size = 384
         self.image_size = 1500
+        self.augment = augment
         self.indices = []
+
+        self.geometric_transform = T.Compose(
+            [
+                T.RandomRotation(30),
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+            ]
+        )
+
+        self.color_transform = T.Compose(
+            [
+                T.ColorJitter(brightness=0.2, contrast=0.2),
+                T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+            ]
+        )
 
         ### Allows 9 patches 384x384 for each 1500x1500 original to be used, only compute once the double loop
         for i in range(len(self.image_paths)):
             for y in range(0, self.image_size, self.crop_size):
                 for x in range(0, self.image_size, self.crop_size):
-                    if y + self.crop_size <= self.image_size and x + self.crop_size <= self.image_size:
+                    if (
+                        y + self.crop_size <= self.image_size
+                        and x + self.crop_size <= self.image_size
+                    ):
                         self.indices.append((i, x, y))
 
     def __len__(self):
-        return len(self.indices)  ### allows all crops to be used without change to dataloading or batch handling
+        return len(
+            self.indices
+        )  ### allows all crops to be used without change to dataloading or batch handling
 
     def __getitem__(self, idx):
         image_idx, x, y = self.indices[idx]
@@ -75,10 +130,18 @@ class MassachusettsDataset(Dataset):
         mask /= 255.0
 
         # crop to 384x384 as used in main dataset
-        image_crop = image[y:y + self.crop_size, x:x + self.crop_size, :]
-        mask_crop = mask[y:y + self.crop_size, x:x + self.crop_size]
+        image_crop = image[y : y + self.crop_size, x : x + self.crop_size, :]
+        mask_crop = mask[y : y + self.crop_size, x : x + self.crop_size]
 
-        #CHW for torch
+        # CHW for torch
         image_crop = np.transpose(image_crop, (2, 0, 1))
 
-        return torch.from_numpy(image_crop), torch.from_numpy(mask_crop)
+        image_tensor = torch.from_numpy(image_crop)
+        mask_tensor = torch.from_numpy(mask_crop)
+
+        if self.augment == "geometric":
+            image_tensor = self.geometric_transform(image_tensor)
+        elif self.augment == "color":
+            image_tensor = self.color_transform(image_tensor)
+
+        return image_tensor, mask_tensor
