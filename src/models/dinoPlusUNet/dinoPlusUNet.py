fd896ee0e80c5da5a2469f8dee3fdfa3c581f811
louis <louis.niederloehner@gmail.com>
2024-07-08 14:34:29 +0200

    DinoPlus embedding, some conv, combine with encoded UNet features, decode together - ugly version. Not terrible, not great.

--- src/models/dinoPlusUNet/dinoPlusUNet.py
+++ src/models/dinoPlusUNet/dinoPlusUNet.py
@@ -26,21 +26,32 @@ class ConvBlock(nn.Module):
         return self.block(x)
 
 class UNetClassifier(torch.nn.Module):
-    def __init__(self, in_channels, tokenW=384, tokenH=384, num_labels=1, chs=(6, 64, 128, 256, 512, 1024)):
+    def __init__(self, in_channels, tokenW=384, tokenH=384, num_labels=1, chs=(3, 64, 128, 256, 512, 1024)):
         super(UNetClassifier, self).__init__()
 
         enc_chs = chs  # number of channels in the encoder
-        dec_chs = chs[::-1][:-1]  # number of channels in the decoder
+        # dec_chs = chs[::-1][:-1]  # number of channels in the decoder
+        upconv_chs_in = [1792, 702, 303, 140, 67]
+        upconv_chs_out = [896, 351, 151, 70, 33]
+        dec_chs_in = [1408, 607, 279, 134, 67]
+        dec_chs_out = [702, 303, 140, 67, 67]
+
 
         self.in_channels = in_channels
         self.width = tokenW
         self.height = tokenH
 
-        self.init_upsampling1 = nn.ConvTranspose2d(in_channels=self.in_channels, out_channels=384, stride=7, kernel_size=14, padding=2)
-        self.init_upsampling2 = nn.ConvTranspose2d(in_channels=384, out_channels=96, stride=2,
-                                                   kernel_size=4, padding=1)
-        self.linear = nn.Conv2d(in_channels=96, out_channels=3, stride=1,
-                                                   kernel_size=3, padding=1)
+        # self.init_upsampling1 = nn.ConvTranspose2d(in_channels=self.in_channels, out_channels=384, stride=7, kernel_size=14, padding=2)
+        # self.init_upsampling2 = nn.ConvTranspose2d(in_channels=384, out_channels=96, stride=2,
+        #                                            kernel_size=4, padding=1)
+        # self.linear = nn.Conv2d(in_channels=96, out_channels=3, stride=1,
+        #                                            kernel_size=3, padding=1)
+
+        self.linear1 = nn.Conv2d(in_channels=768, out_channels=768, stride=1,
+                                                    kernel_size=3, padding=0)
+
+        self.linear2 = nn.Conv2d(in_channels=768, out_channels=768, stride=1,
+                                 kernel_size=2, padding=0)
 
         self.enc_blocks = nn.ModuleList(
             [ConvBlock(in_ch, out_ch) for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[1:])]
@@ -48,15 +59,15 @@ class UNetClassifier(torch.nn.Module):
         self.pool = nn.MaxPool2d(2)
         self.upconvs = nn.ModuleList([
                 nn.ConvTranspose2d(in_ch, out_ch, 2, 2)
-                for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])
+                for in_ch, out_ch in zip(upconv_chs_in, upconv_chs_out)
             ])
         # deconvolution
         self.dec_blocks = nn.ModuleList(
-            [ConvBlock(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])]
+            [ConvBlock(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs_in, dec_chs_out)]
         )  # decoder blocks
 
         self.head = nn.Sequential(
-            nn.Conv2d(dec_chs[-1], 1, 1), nn.Sigmoid()
+            nn.Conv2d(dec_chs_out[-1], 1, 1), nn.Sigmoid()
         )  # 1x1 convolution for producing the output-
 
         self.l_classifier = torch.nn.Conv2d(in_channels, num_labels, (1, 1))
@@ -65,13 +76,19 @@ class UNetClassifier(torch.nn.Module):
         embeddings = embeddings.reshape(-1, self.height, self.width, self.in_channels)
         embeddings = embeddings.permute(0, 3, 1, 2)
 
-        x = self.init_upsampling1(embeddings)
-        x = self.init_upsampling2(x)
-        x = nn.functional.pad(x, (1, 1, 1, 1))
-        x = self.linear(x)
+        z = self.linear1(embeddings)
+        z = self.linear2(z)
 
-        x = torch.cat((x, pixel_values), dim=1)
+        # z = nn.functional.pad(z, (1, 0, 1, 0))
 
+        # x = self.init_upsampling1(embeddings)
+        # x = self.init_upsampling2(x)
+        # x = nn.functional.pad(x, (1, 1, 1, 1))
+        # x = self.linear(x)
+
+        # x = torch.cat((x, pixel_values), dim=1)
+
+        x = pixel_values
 
         enc_features = []
         for block in self.enc_blocks[:-1]:
@@ -79,6 +96,10 @@ class UNetClassifier(torch.nn.Module):
             enc_features.append(x)  # save features for skip connections
             x = self.pool(x)  # decrease resolution
         x = self.enc_blocks[-1](x)
+
+        #combine
+        x = torch.cat((x, z), 1)
+
         # decode
         for block, upconv, feature in zip(
                 self.dec_blocks, self.upconvs, enc_features[::-1]
