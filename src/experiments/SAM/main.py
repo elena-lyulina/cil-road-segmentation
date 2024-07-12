79c87801357a6509b077b8805d7719fee52ff08b
louis <louis.niederloehner@gmail.com>
2024-07-12 13:11:55 +0200

    Add batches to SAM; Data processing done in forward function, change pipeline if we stick with SAM

--- src/experiments/SAM/main.py
+++ src/experiments/SAM/main.py
@@ -12,11 +12,11 @@ cur_config = {
     },
     "dataset": {
         "name": "cil",
-        "params": {"batch_size": 1, "shuffle": True, "resize_to": (400, 400), "augment": ["geometric"]},
+        "params": {"batch_size": 4, "shuffle": True, "resize_to": (400, 400), "augment": ["geometric"]},
     },
     "train": {
         "n_epochs": 30,
-        "optimizer": {"name": "Adam", "params": {"lr": 0.0005}},
+        "optimizer": {"name": "Adam", "params": {"lr": 0.00001}},
         "loss": {"name": "BCELoss", "params": {}},
         "clip_grad": None,
     },
